7fa0a8752477c5b9ddc1072916d0380c648cd779
Ronie <roniemeque@icloud.com>
2020-10-17 18:55:29 -0300

    warning on ifttt

--- src/handler.ts
+++ src/handler.ts
@@ -1,10 +1,33 @@
 import { APIGatewayProxyHandler } from "aws-lambda";
 import { scrapLastResultForCpf } from "./scrap";
+import fetch from "node-fetch";
+
+const KEY = "";
 
 const answerApi = (body: any, statusCode = 200) => ({
   statusCode,
   body: JSON.stringify(body),
 });
+
+const sendToPhone = async (time?: string, message?: string) => {
+  if (!time || !message) {
+    return;
+  }
+
+  return await fetch(
+    `https://maker.ifttt.com/trigger/status_checked/with/key/${KEY}`,
+    {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        value1: time,
+        value2: message,
+      }),
+    }
+  );
+};
 
 export const fetchStatus: APIGatewayProxyHandler = async (event, context) => {
   try {
@@ -19,7 +42,11 @@ export const fetchStatus: APIGatewayProxyHandler = async (event, context) => {
       );
     }
 
-    const { time, message } = await scrapLastResultForCpf(cpf);
+    const { time, message } = process.env.IS_OFFLINE
+      ? { time: "", message: "" }
+      : await scrapLastResultForCpf(cpf);
+
+    await sendToPhone(time, message);
 
     return answerApi({ time, message });
   } catch (error) {
